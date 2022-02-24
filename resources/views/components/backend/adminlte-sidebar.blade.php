100b4d37c2f8df25585e97a6c365d5f382f10c56
mvishu405 <36050287+mvishu405@users.noreply.github.com>
2022-02-24 20:51:51 +0530

    done the npm run watch

--- resources/views/components/backend/adminlte-sidebar.blade.php
+++ resources/views/components/backend/adminlte-sidebar.blade.php
@@ -4,15 +4,7 @@
     <ul class="navbar-nav">
         <li class="nav-item"> <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a> </li>
     </ul>
-    <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
-        <div class="input-group input-group-sm">
-            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit"> <i class="fas fa-search"></i> </button>
-            </div>
-        </div>
-    </form>
+
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
@@ -31,13 +23,13 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link"> <img src="{{asset('backend/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> <span class="brand-text font-weight-light">AdminLTE 3</span> </a>
+    <a href="index3.html" class="brand-link"> <img src="{{asset('backend/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span> </a>
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image"> <img src="{{asset('backend/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image"> </div>
-            <div class="info"> <a href="#" class="d-block">Alexander Pierce</a> </div>
+            <div class="info"> <a href="#" class="d-block">{{ Auth::user()->email }}</a> </div>
         </div>
         <!-- Sidebar Menu -->
         <nav class="mt-2">
