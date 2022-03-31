0f7d2da88613b42ef3548b89d8a7d4e433a9f93b
VipVishal007 <mvishu405@gmail.com>
2022-03-31 23:25:50 +0530

    done the contact and admin panel

--- resources/views/backend/contact/index.blade.php
+++ resources/views/backend/contact/index.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="card card-primary card-outline">
             <div class="card-header">
-                <h3 class="card-title">Career List</h3>
+                <h3 class="card-title">Contact List</h3>
                 <div class="card-tools">
                     {{-- <button type="button" class="btn btn-sm btn-primary"><i class="fas fa-plus"></i></button> --}}
                 </div>
@@ -17,27 +17,19 @@
                             <th>Name</th>
                             <th>Mobile number</th>
                             <th>Email</th>
-                            <th>Position</th>
-                            <th>Showreel link</th>
                             <th>Message</th>
-                            <th>CV</th>
                             <th>Created Date</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($careers as $career)
+                        @foreach ($contacts as $contact)
                             <tr>
-                                <td>{{ $career->id }}</td>
-                                <td>{{ $career->name }}</td>
-                                <td>{{ $career->mobile_number }}</td>
-                                <td>{{ $career->emailId }}</td>
-                                <td>{{ $career->position }}</td>
-                                <td>{{ $career->showreel_link }}</td>
-                                <td>{{ $career->message }}</td>
-                                <td>
-                                    <a href="{{ viewFile($career->cv) }}" target="_blank">Download</a>
-                                </td>
-                                <td>{{ $career->created_at }}</td>
+                                <td>{{ $contact->id }}</td>
+                                <td>{{ $contact->name }}</td>
+                                <td>{{ $contact->mobile_number }}</td>
+                                <td>{{ $contact->emailId }}</td>
+                                <td>{{ $contact->message }}</td>
+                                <td>{{ $contact->created_at }}</td>
                             </tr>
                         @endforeach
                     </tbody>
