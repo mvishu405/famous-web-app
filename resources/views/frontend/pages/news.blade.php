d0e8a8e5c5c0696b462316b26312cc6ec08e37bf
VipVishal007 <mvishu405@gmail.com>
2022-04-01 11:11:50 +0530

    done

--- resources/views/frontend/pages/news.blade.php
+++ resources/views/frontend/pages/news.blade.php
@@ -32,20 +32,20 @@
                 <div class="tab_section_inner">
                     <ul class="service_tabs">
                         <!-- <li class="active">
-                                        <a href="javascript:void(0)">All</a>
-                                    </li>
-                                    <li class="">
-                                        <a href="javascript:void(0)">Famous Studio</a>
-                                    </li>
-                                    <li class="">
-                                        <a href="javascript:void(0)">Famous House of artists</a>
-                                    </li>
-                                    <li class="">
-                                        <a href="javascript:void(0)">Famous Working Company</a>
-                                    </li>
-                                    <li class="">
-                                        <a href="javascript:void(0)">Awards</a>
-                                    </li> -->
+                                    <a href="javascript:void(0)">All</a>
+                                </li>
+                                <li class="">
+                                    <a href="javascript:void(0)">Famous Studio</a>
+                                </li>
+                                <li class="">
+                                    <a href="javascript:void(0)">Famous House of artists</a>
+                                </li>
+                                <li class="">
+                                    <a href="javascript:void(0)">Famous Working Company</a>
+                                </li>
+                                <li class="">
+                                    <a href="javascript:void(0)">Awards</a>
+                                </li> -->
                     </ul>
                     <div class="filter">
                         <select name="latest" id="latest">
@@ -59,23 +59,24 @@
         <section class="news_section">
             <div class="container">
                 <div class="news_wrap">
+
                     @foreach ($news as $ns)
                         <div class="news_item fade_in col-md-4">
-                            <a href="{{ $ns->news_link }}" class="news_item_inner" target="_blank">
+                            <a href="{{ $ns->news_link }}"
+                                class="news_item_inner" target="_blank">
                                 <div class="news_image">
                                     <img src="{{ viewFile($ns->preview_image) }}" alt="" class="img-responsive">
                                 </div>
                                 <div class="news_content">
                                     <div class="blog_header">
                                         <div class="date_min_read text_xs">
-                                            <span
-                                                class="date">{{ Carbon\Carbon::parse($ns->news_date)?->toFormattedDateString() }}</span>
+                                            <span class="date">{{ Carbon\Carbon::parse($ns->news_date)?->toFormattedDateString() }}</span>
                                             <span class="dot"></span>
                                             <span class="min_read">Work</span>
                                         </div>
                                     </div>
                                     <h2 class="blog_title text_sm">
-                                        {!! $ns->description !!}
+                                        {{ strip_tags($ns->description) }}
                                     </h2>
                                 </div>
                                 <div class="news_source">
@@ -84,6 +85,10 @@
                             </a>
                         </div>
                     @endforeach
+
+
+
+
                 </div>
             </div>
         </section>
@@ -101,9 +106,10 @@
         $('select#latest').on('change', function(e) {
             var optionSelected = $("option:selected", this);
             var valueSelected = this.value;
-//             console.log(optionSelected, valueSelected, location.protocol + '//' + location.host + location.pathname
-// );
-            window.location.href = location.protocol + '//' + location.host + location.pathname+'?'+valueSelected;
+            //             console.log(optionSelected, valueSelected, location.protocol + '//' + location.host + location.pathname
+            // );
+            window.location.href = location.protocol + '//' + location.host + location.pathname + '?' +
+                valueSelected;
         });
     </script>
 @endpush
