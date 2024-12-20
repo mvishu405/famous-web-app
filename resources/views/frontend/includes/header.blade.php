a353c80a3191f491de9d5d3d22fdb66dc14f8744
Shikha Dubey <shikha.dubey@ting.in>
2024-12-20 13:07:07 +0530

    changes the menu in mobile

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -77,5 +77,89 @@
 </header>
 
 <div class="dragCursor">
-    <img src="img/drag_btn.png" alt="" class="img-responsive" />
+    <img src="{{ asset('img/drag_btn.png')}}" alt="" class="img-responsive" />
 </div>
+<div
+  id="commonSliderArrowLinks"
+  data-left-arrow-path="{{ asset('img/arrow_left.svg')}}"
+  data-right-arrow-path="{{ asset('img/arrow_right.svg')}}"
+></div>
+
+<script>
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+</script>
+
+<script>
+  // function toggleDropdown(arrow) {
+  //   const parent = arrow.closest(".menu-item"); // Parent menu-item
+  //   const submenu = parent.querySelector(".submenu"); // Dropdown menu
+  //   const menuBlock = parent.closest(".menu-block"); // The menu block container
+  //   const nextItem = parent.nextElementSibling; // Item directly below the dropdown
+
+  //   // Toggle the dropdown visibility
+  //   submenu.classList.toggle("open");
+
+  //   // Toggle the arrow rotation
+  //   arrow.classList.toggle("rotated");
+
+  //   // Adjust the margin for the item directly below the dropdown
+  //   if (submenu.classList.contains("open")) {
+  //     submenu.style.maxHeight = submenu.scrollHeight + "px";
+  //     nextItem.style.marginTop = submenu.scrollHeight + "px"; // Push only the next item
+  //   } else {
+  //     submenu.style.maxHeight = "0px";
+  //     nextItem.style.marginTop = "0px"; // Reset margin-top when closed
+  //   }
+  // }
+  function toggleDropdown(arrow) {
+    const parent = arrow.closest(".menu-item"); // Parent menu-item
+    const submenu = parent.querySelector(".submenu"); // Current submenu
+    const menuBlock = document.querySelector(".menu-block"); // Entire menu block
+
+    // Close all other open dropdowns
+    const openDropdowns = menuBlock.querySelectorAll(".submenu.open");
+    openDropdowns.forEach((dropdown) => {
+      if (dropdown !== submenu) {
+        dropdown.classList.remove("open");
+        dropdown.style.maxHeight = "0px";
+
+        const dropdownParent = dropdown.closest(".menu-item");
+        const dropdownNextItem = dropdownParent.nextElementSibling;
+        if (dropdownNextItem) {
+          dropdownNextItem.style.marginTop = "0px"; // Reset margin for other dropdowns
+        }
+
+        // Reset rotation for arrows of other dropdowns
+        const otherArrow = dropdownParent.querySelector(".dropdownArrow");
+        if (otherArrow) {
+          otherArrow.classList.remove("rotated");
+        }
+      }
+    });
+
+    // Toggle the current dropdown visibility
+    submenu.classList.toggle("open");
+    arrow.classList.toggle("rotated");
+
+    // Adjust margin for the next item
+    const nextItem = parent.nextElementSibling;
+    if (submenu.classList.contains("open")) {
+      submenu.style.maxHeight = submenu.scrollHeight + "px";
+      if (nextItem) {
+        nextItem.style.marginTop = submenu.scrollHeight + "px"; // Push the next item
+      }
+    } else {
+      submenu.style.maxHeight = "0px";
+      if (nextItem) {
+        nextItem.style.marginTop = "0px"; // Reset margin when closed
+      }
+    }
+  }
+</script>
