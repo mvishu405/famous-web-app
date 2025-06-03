e96ad011b448cfacefddccd6a2cdece557bddc55
gitrangers <gitrangers@gmail.com>
2025-06-03 15:20:03 +0530

    Wrap team description in a div for improved styling and layout consistency

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -549,7 +549,9 @@
                     <div class="col-xs-12 col-sm-6 col-md-9">
                         <h3 class="text_lg text_600">{{ $team->name }}</h3>
                         <h4 class="text_500 text_blue team_popup_desc">{{ $team->designation }}</h4>
-                        {!! $team->description !!}
+                        <div class="leader_contant">
+                         {!! $team->description !!}
+                       </div>
                     </div>
                 </div>
             </div>
