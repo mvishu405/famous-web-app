766c2887537511a78920d57e87b4b9c1cc85a8d6
gitrangers <gitrangers@gmail.com>
2025-07-30 18:42:19 +0530

    done

--- resources/views/frontend/includes/contact-section.blade.php
+++ resources/views/frontend/includes/contact-section.blade.php
@@ -24,8 +24,8 @@
                             <div class="ldi_item_inner">
                                 <h2 class="ldi_title text_xs">Address</h2>
                                 <p class="ldi_cont">
-                                    <strong>Famous Studios Ltd. ,</strong>
-                                    Famous Cine Building, 
+                                    <strong>Famous Studios Ltd.</strong>
+                                    Famous Cine Building, <br />
                                     20 Dr E Moses Road, <br />
                                     Mahalaxmi,  Mumbai - 400 011
                                 </p>
